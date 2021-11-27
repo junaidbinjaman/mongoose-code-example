927399370b60111e7f2a6fe112fe02a332dece1c
iMac <imac@jwolts-iMac.local>
2021-11-27 18:36:11 +0600

    fghfg

--- index.js
+++ index.js
@@ -1,9 +1,40 @@
-const fs = require('fs');
+const http = require("http");
 
-const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`,);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write('<html><head><title>Form</title></head>');
+    res.write(
+        '<body><form method="post" action="/process"><input name="message" /></form></body>'
+    );
+    res.end();
+  } else if (req.url === "/process" && req.method === 'POST') {
+    const body = []
+    req.on('data', (chunk) => {
+      body.push(chunk)
+    })
+    req.on('end', () => {
+      console.log('stream finished');
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+    })
+    res.write("Thank you for submitting");
+    res.end();
+  } else {
+    res.write("Page not found");
+    res.end();
+  }
+});
 
-ourReadStream.on('data', (chunk) => {
-  console.log(chunk);
-})
+server.listen(3000);
 
-console.log('Hello,');
+console.log("listening on port 3000");
+
+// const fs = require('fs');
+
+// const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`,);
+
+// ourReadStream.on('data', (chunk) => {
+//   console.log(chunk);
+// })
+
+// console.log('Hello,');
